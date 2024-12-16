60c0192985a46e97f629b72a54b760be5191ae89
김회준 <rjcnd123@gmail.com>
2024-12-16 16:02:46 +0900

    add zed configs

---
+++ files/workspace/.config/zed/custom_runfile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Access the full path using ZED_FILE
+full_path="$ZED_FILE"
+
+# Extract filename with extension
+filename_ext=$(basename "$full_path")
+
+# Extract filename and extension
+filename="${filename_ext%.*}"
+extension="${filename_ext##*.}"
+
+if [ "$extension" == "sh" ]; then
+    source "$full_path";
+elif [ $filename != "flake.nix" -a "$extension" == "nix" ]; then
+    nix eval -f "$full_path";
+elif [ "$extension" == "py" ]; then
+    python3 "$full_path";
+else
+    echo "run file: Not defined."
+fi
--- homes/default.nix
+++ homes/default.nix
@@ -4,8 +4,4 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  nix.settings.trusted-users = [
-    "root"
-    myOptions.userName
-  ];
 }
