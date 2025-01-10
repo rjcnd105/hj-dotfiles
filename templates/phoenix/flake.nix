4a8321554e5f78071384cdaf734c00678dbd10cc
김회준 <rjcnd123@gmail.com>
2025-01-10 12:46:28 +0900

    devtool들 mise기반으로 변경 +  nix flake, devnev 기반 phoenix template 초안 (#3)
    * remove fuck env
    
    * mise config.toml을 통한 global 의존성 관리
    
    * mise package github release branch 바라보게
    
    * add mise tools
    
    * change
    
    * test
    
    * add mise install form hm dag activate
    
    * mise base tool management
    
    * update readme

--- templates/phoenix/flake.nix
+++ templates/phoenix/flake.nix
@@ -7,7 +7,6 @@
       url = "github:jdx/mise/release";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
   outputs =
     inputs@{
@@ -18,6 +17,7 @@
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
         inputs.devenv.flakeModule
       ];
       systems = [
@@ -29,34 +29,124 @@
 
       perSystem =
         {
-          config,
-          self',
-          inputs',
-          pkgs,
           system,
+          lib,
+          config,
           ...
         }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
+        let
+          overlayedPkgs = import nixpkgs {
+            system = system;
             overlays = [
               (final: prev: {
                 mise = prev.callPackage (mise + "/default.nix") { };
               })
             ];
+            config = {
+              allowUnfreePredicate =
+                pkg:
+                builtins.elem (lib.getName pkg) [
+                  "timescaledb"
+                ];
+            };
           };
-          packages.default = [
-            pkgs.gh
-            pkgs.hello
-            pkgs.mise
-          ];
+        in
+        {
+          _module.args.pkgs = overlayedPkgs;
 
-          devenv.shells.default = {
-            # https://devenv.sh/reference/options/
-            packages = [ config.packages.default ];
-            imports = [ ./devenv.nix ];
+          devenv.shells.default =
+            {
+              config,
+              lib,
+              pkgs,
+              ...
+            }:
+            let
+              inherit (config.env) APP_NAME DB_NAME;
+            in
+            {
 
-          };
+              dotenv = {
+                enable = true;
+                filename = [
+                  ".env.dev"
+                ];
+              };
+
+              env.MISE_GLOBAL_CONFIG = false;
+              env.MISE_DEFAULT_TOOL_VERSIONS_FILENAME = ".mise.toml";
+
+              # services
+              services.postgres = {
+                enable = true;
+                initialScript = ''
+                  CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres' SUPERUSER;
+                '';
+                initialDatabases = [ { name = config.env.APP_NAME; } ];
+                extensions = extensions: [
+                  extensions.postgis
+                  extensions.timescaledb
+                ];
+                initdbArgs = [
+                  "--locale=ko_KR.UTF-8"
+                  "--encoding=UTF8"
+                ];
+                package = pkgs.postgresql_17;
+              };
+
+              services.caddy = {
+                enable = true;
+                package = pkgs.caddy;
+              };
+
+              # services.opentelemetry-collector = {
+              #   enable = true;
+              #   package = pkgs.opentelemetry-collector-contrib;
+              # };
+
+              processes.phoenix.exec = "cd hello && mix phx.server";
+
+              tasks."myapp:hello" = {
+                exec = ''echo "Hello, world!"'';
+                before = [
+                  "devenv:enterShell"
+                  "devenv:enterTest"
+                ];
+              };
+
+              packages =
+                [
+                  # https://devenv.sh/reference/options/
+                  pkgs.mise
+                ]
+                ++ lib.optionals pkgs.stdenv.isLinux [
+                  pkgs.inotify-tools
+                ]
+                ++ lib.optionals (!config.container.isBuilding) [
+                ];
+
+              scripts = {
+                "mise-init" = {
+                  exec = ''
+                    mise trust ./.
+                    mise install
+                    mise activate -q
+                  '';
+                };
+                "env-info" = {
+                  exec = ''
+                    echo your system: ${system}
+                    echo your app: ${APP_NAME}
+                    echo your DB_NAME: ${DB_NAME}
+                  '';
+                };
+              };
+
+              enterShell = ''
+                echo hello
+              '';
+
+            };
         };
     };
 }
