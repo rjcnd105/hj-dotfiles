9147e1330808b6a18433ecf1c8f039a05106aa29
김회준 <rjcnd123@gmail.com>
2025-04-04 14:30:37 +0900

    add mise/jj

--- files/workspace/.config/zed/languages/elixir/injections.scm
+++ files/workspace/.config/zed/languages/elixir/injections.scm
@@ -6,3 +6,11 @@
   (quoted_content) @injection.content
  (#eq? @_sigil_name "V")
  (#set! injection.language "svelte"))
+
+
+; heex
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "H")
+ (#set! injection.language "heex"))
