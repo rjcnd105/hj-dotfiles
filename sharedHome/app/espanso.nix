ca63c381a752d01acdd42719fd5571a312e4b122
김회준 <rjcnd123@gmail.com>
2025-05-09 12:33:00 +0900

    update settings

--- sharedHome/app/espanso.nix
+++ sharedHome/app/espanso.nix
@@ -1,24 +1,5 @@
 {
   programs.espanso = {
     enable = true;
-
-    global_vars = {
-      global_vars = [
-        {
-          name = "currentdate";
-          type = "date";
-          params = {
-            format = "%Y.%m.%d";
-          };
-        }
-        {
-          name = "currenttime";
-          type = "date";
-          params = {
-            format = "%H:%M:%S";
-          };
-        }
-      ];
-    };
   };
 }
