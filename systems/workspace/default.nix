362777ebd21308091bfd3a3c0bb12cec2460e81c
김회준 <rjcnd123@gmail.com>
2025-08-29 15:13:17 +0900

    update configs

--- systems/workspace/default.nix
+++ systems/workspace/default.nix
@@ -6,11 +6,11 @@
   ...
 }:
 let
-  variables = {
-    USER = myOptions.userName;
-    EDITOR = "zed";
-    LANG = "ko_KR.UTF-8";
-  };
+  # variables = {
+  #   USER = myOptions.userName;
+  #   EDITOR = "zed";
+  #   LANG = "ko_KR.UTF-8";
+  # };
 in
 {
 
@@ -61,26 +61,9 @@ in
 
     };
 
+    # https://determinate.systems/blog/nix-darwin-updates/#what-you-should-change
+    # TODO: 그러나 이후 https://determinate.systems/blog/changelog-determinate-nix-386/#nix-darwin 적용 확인..
     nix.enable = false;
-    # nix.package = pkgs.nix;
-    # nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
-    # nix.settings = {
-
-    #   trusted-users = [
-    #     "root"
-    #     "@wheel"
-    #   ];
-    #   extra-trusted-substituters = "https://cache.flakehub.com";
-    #   extra-trusted-public-keys = "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=";
-
-    #   upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
-
-    #   netrc-file = "/nix/var/determinate/netrc";
-    #   extra-substituters = "https://cache.flakehub.com";
-
-    #   experimental-features = "nix-command flakes";
-    #   always-allow-substitutes = "true";
-    # };
 
     home-manager = {
       sharedModules = [
